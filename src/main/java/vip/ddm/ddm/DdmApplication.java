2ec087e38802e33222058435c1480e2a1abc0630
haoyuehong <799550298@qq.com>
2018-09-05 23:32:41 +0800

    完成商品分类 和 商品模块

--- src/main/java/vip/ddm/ddm/DdmApplication.java
+++ src/main/java/vip/ddm/ddm/DdmApplication.java
@@ -1,11 +1,16 @@
 package vip.ddm.ddm;
 
+import com.github.tobato.fastdfs.FdfsClientConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableMBeanExport;
+import org.springframework.context.annotation.Import;
+import org.springframework.jmx.support.RegistrationPolicy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@Import(FdfsClientConfig.class)
 @SpringBootApplication
 @EnableTransactionManagement
 @MapperScan("vip.ddm.ddm.dao")
---
+++ src/main/java/vip/ddm/ddm/controller/UploadController.java
@@ -0,0 +1,23 @@
+package vip.ddm.ddm.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import vip.ddm.ddm.common.FastDFSClientWrapper;
+import vip.ddm.ddm.result.Result;
+
+import java.io.IOException;
+
+@RestController
+public class UploadController {
+
+    @Autowired
+    private FastDFSClientWrapper dfsClient;
+
+    @RequestMapping(value = "/upload",method = RequestMethod.POST)
+    public Result upload(MultipartFile file) throws IOException {
+        return Result.success(dfsClient.uploadFile(file));
+    }
+}
